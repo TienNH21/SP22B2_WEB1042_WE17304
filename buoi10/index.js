39f347dd87ae0d407828a5f8f308eb115e7a5c96
HoangTien339 <tiennh21@fpt.edu.vn>
2022-04-08 17:35:58 +0700

    CRUD Full

--- buoi10/index.js
+++ buoi10/index.js
@@ -19,6 +19,8 @@ let listSV = [
     },
 ];
 
+let viTri = -1;
+
 function them() {
     let hoTen = document.getElementById("ho_ten").value;
     let maSV = document.getElementById("ma_sv").value;
@@ -50,13 +52,60 @@ function loadTable() {
                 <td>${ sv.ma_sv }</td>
                 <td>${ sv.chuyen_nganh }</td>
                 <td>${ sv.hoc_ky }</td>
+                <td>
+                    <button onclick="capNhat(${ i })">
+                        Cập nhật
+                    </button>
+                </td>
+                <td>
+                    <button onclick="xoa(${ i })">
+                        Xóa
+                    </button>
+                </td>
             </tr>
         `;
         html = html + tableRow;
     }
-    console.log(html);
     document.getElementById("table_body")
         .innerHTML = html;
+}
+
+function capNhat(i) {
+    viTri = i;
+    let sv = listSV[i];
+
+    document.getElementById("ho_ten").value = sv.ho_ten;
+    document.getElementById("ma_sv").value = sv.ma_sv;
+    document.getElementById("chuyen_nganh").value = sv.chuyen_nganh;
+    document.getElementById("hoc_ky").value = sv.hoc_ky;
+}
+
+function xoa(i) {
+    listSV.splice(i, 1);
+    loadTable();
+}
+
+function luu() {
+    if (viTri == -1) {
+        them();
+    } else {
+        capNhatArray();
+    }
+}
+
+function capNhatArray() {
+    let hoTen = document.getElementById("ho_ten").value;
+    let maSV = document.getElementById("ma_sv").value;
+    let chuyenNganh = document.getElementById("chuyen_nganh").value;
+    let hocKy = document.getElementById("hoc_ky").value;
+
+    listSV[viTri].ho_ten = hoTen;
+    listSV[viTri].ma_sv = maSV;
+    listSV[viTri].chuyen_nganh = chuyenNganh;
+    listSV[viTri].hoc_ky = hocKy;
+
+    viTri = -1;
+    loadTable();
 }
 
 loadTable();
